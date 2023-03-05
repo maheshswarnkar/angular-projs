b717f28fdc1eb1941edc8b60da20fc0cd0251646
Mahesh <mahesh.swarnkar1984@gmail.com>
2023-03-05 22:14:32 +0530

    first commit

---
+++ angular-notes.c
@@ -0,0 +1,37 @@
+Check Node version:- 
+	node -v
+	v18.14.1
+Check NPM version:- 
+	npm -v
+	9.3.1
+
+"Angular CLI command": - 
+	npm install -g @angular/cli
+	ng new my-dream-app
+	cd my-dream-app
+	ng serve
+
+"Create First Project":-
+	command - 
+		"ng new firstProject"
+
+"Start project":- 
+	"ng serve" [Go inside of directory and then run this command]
+
+"URL":-
+	"http://localhost:4200/"
+
+
+"ts":- typescript
+
+"Create New component":-
+	'ng generate component hello-world'
+	
+"Key board shortcuts":-
+	'To Beautify':- "ALT + Shift +F"
+	'To Copy Line':- "Shift + Alt + Up/Down arroy keys"
+	'Delete Line':- "Ctrl + X"
+	
+	
+	
+	
