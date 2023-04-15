4140aec2302ac8b29a2d139be200e07271da4a71
Mahesh <mahesh.swarnkar1984@gmail.com>
2023-04-15 21:07:32 +0530

    changes regarding displaying logs

--- angular-notes.c
+++ angular-notes.c
@@ -1,3 +1,10 @@
+Angular 6 URL You Tube URL:- 
+
+	https://www.youtube.com/watch?v=CacMOuzTiJU&list=PLqq-6Pq4lTTb7JGBTogaJ8bm7f8VCvFkj&index=14
+	
+
+---------------------------------------------------------------------------------------------------
+
 Check Node version:- 
 	node -v
 	v18.14.1
@@ -10,6 +17,13 @@ Check NPM version:-
 	ng new my-dream-app
 	cd my-dream-app
 	ng serve
+"To update Angular":- 
+	npm update -g
+	npm update -g <package_name>
+"Create Module":-
+	ng generate module <Module_Name>
+"Create Component":-
+	ng generate component <Component_Name>
 
 "Create First Project":-
 	command - 
@@ -32,6 +46,73 @@ Check NPM version:-
 	'To Copy Line':- "Shift + Alt + Up/Down arroy keys"
 	'Delete Line':- "Ctrl + X"
 	
-	
+---------------------------------------------------------------------------------------------------
+
+
+20230306173202136552100304	
+
+
+logging.level.root=WARN
+logging.level.com.ms.randomdata.api.controller=TRACE
+
+
+
+ms-tech-system-spring-boot-aws
+
+---------------------------------------------------------------------------------------------------
+
+https://www.techgig.com/practice/java/introduction
+
+---------------------------------------------------------------------------------------------------
+
+
+
+fisher
+Barak
+
+---------------------------------------------------------------------------------------------------
+{
+  "name": "Launch Chrome",
+  "request": "launch",
+  "type": "chrome",
+  "url": "http://localhost:4200",
+  "webRoot": "${workspaceFolder}"
+},
+{
+  "name": "ng serve",
+  "type": "pwa-chrome",
+  "request": "launch",
+  "preLaunchTask": "npm: start",
+  "url": "http://localhost:4200/"
+},
+{
+  "name": "ng test",
+  "type": "chrome",
+  "request": "launch",
+  "preLaunchTask": "npm: test",
+  "url": "http://localhost:9876/debug.html"
+}
+
+---------------------------------------------------------------------------------------------------
+
+
+Basic overview of OOPs concepts and implementation of DAO and Factory Pattern.
+SOLID principles
+Learn about Creational Design Pattern concepts and application using Java Code Examples.
+Learn about Structural Design Pattern concepts and application using Java Code Examples.
+Learn about Behavioral Design Pattern concepts and application using Java Code Examples.
+Learn about Concurrency Design Pattern concepts and application using Java Code Examples.
+The negative consequences that arises out by applying wrong design patterns to a problem.
+
+
+
+
+
+
+
+
+
+
+
 	
 	
